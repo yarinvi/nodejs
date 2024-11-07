9f29986dcfc97061fbc7790053cf2ec04c31aae1
yarinvi <yarin559900@gmail.com>
2024-11-07 09:17:47 +0200

    get all incomes

--- controllers/income.js
+++ controllers/income.js
@@ -39,6 +39,29 @@ const addIncome = async (req, res) => {
   }
 };
 
+const getIncomes = async (req, res) => {
+  try {
+    const userId = userIdValidation.parse(req.params.userId);
+
+    const userExists = await User.findById(userId);
+    if (!userExists) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const incomes = await Income.find({_id: {$in: userExists.incomes}});
+    
+    return res.status(200).json(incomes);
+
+  } catch (error) {
+    console.log(error);
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({ message: error.errors[0].message });
+    }
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};  
+
+
 module.exports = {
   addIncome,
 };
